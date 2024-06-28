3056281ea62c152567576978aae839d99559aa03
Kasiri <135030892+Kasiri-git@users.noreply.github.com>
2024-06-28 10:14:44 +0900

    Version: 1.1.2
    Version: 1.1.2

--- wp-hashtag-converter.php
+++ wp-hashtag-converter.php
@@ -2,22 +2,19 @@
 /*
 Plugin Name: WP Hashtags for Cocoon
 Description: タグのテキスト部分の最初に＃を付与します。
-Version: 1.1.0
+Version: 1.1.2
 Author: Kasiri
 */
 
-// タグクラウド
 function modify_tag_cloud($tag_string) {
     return preg_replace('/>([^<]+)<\/a>/', '>#$1</a>', $tag_string);
 }
 add_filter('wp_generate_tag_cloud', 'modify_tag_cloud', 10, 1);
 
-// アーカイブタイトルとタグリンク
 function force_add_hash_to_tags_and_archive_title() {
     ?>
     <script>
     document.addEventListener('DOMContentLoaded', function() {
-        // アーカイブタイトルの処理
         var archiveTitle = document.getElementById('archive-title');
         if (archiveTitle) {
             var titleText = archiveTitle.lastElementChild.textContent.trim();
@@ -26,7 +23,6 @@ function force_add_hash_to_tags_and_archive_title() {
             }
         }
 
-        // タグリンクの処理
         var tagLinks = document.querySelectorAll('.entry-tags .tag-link');
         tagLinks.forEach(function(link) {
             var tagIcon = link.querySelector('.tag-icon');
@@ -44,13 +40,11 @@ function force_add_hash_to_tags_and_archive_title() {
 }
 add_action('wp_footer', 'force_add_hash_to_tags_and_archive_title');
 
-// サーバーサイドでのタグリンク処理
 function add_hash_to_tag_links($output, $args) {
     return preg_replace('/<a([^>]+)><span([^>]+)><\/span>\s*([^<]+)<\/a>/', '<a$1><span$2></span> #$3</a>', $output);
 }
 add_filter('wp_tag_cloud', 'add_hash_to_tag_links', 10, 2);
 
-// 個別投稿ページのタグリンク処理
 function add_hash_to_post_tags($tags_list) {
     return preg_replace('/<a([^>]+)><span([^>]+)><\/span>\s*([^<]+)<\/a>/', '<a$1><span$2></span> #$3</a>', $tags_list);
 }
